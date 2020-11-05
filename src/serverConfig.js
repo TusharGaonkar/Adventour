37800dc196cf381d4f881e6e5655f2e43d8d442e
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2020-11-05 19:04:24 +0530

    addes Demo routes For Get.Post,Patch,Delete

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
-
-app.set('view engine','hbs');
-
 const app = express();
+const serverPort = 2400;
+app.use(express.json());
+const demoJson = {
+
+  success : true,
+  age : 21,
+  data : {
+    dataitems : "data"
+  }
+};
+
+const callBackfunction = (req , res) => {
+
+  res.status(200).send(demoJson);
+}
+
+
+
 const publicDirectory = path.join(__dirname, '../public');
 
 
-app.get('' , (req ,res)=>{
- res.send("Hello world fdfdf=dsddikmdfkmdkftushar")
+app.get('', callBackfunction);
+
+
+app.get('/api/v1.0/tours' , callBackfunction);
+app.post('/api/v1.0/tours' , callBackfunction);
+
+
+
+app.get('/api/v1.0/tours/:id' , callBackfunction);
+
+app.patch('/api/v1.0/tours/:id' , callBackfunction);
+app.delete('/api/v1.0/tours/:id', callBackfunction); 
+
+
+app.listen(serverPort,() => {
+  
+  console.log(chalk.inverse.green(`Server Started at port ${serverPort}`));
 })
 
-app.listen(3000, ()=> {
- 
-  console.log(chalk.inverse.green('Server is up and running on port 3000'));
-})
 
 
 
